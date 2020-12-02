8d4e68976772e8d044c5611221d83a463ba98755
Chet Murthy <chetsky@gmail.com>
2020-12-02 15:37:56 -0800

    meh

--- pa_q_ast/META.pl
+++ pa_q_ast/META.pl
@@ -25,7 +25,7 @@ description = "pa_ppx_q_ast deriver"
     archive(syntax,preprocessor,native) = "pa_deriving_q_ast.cmx"
 
   package "link" (
-  requires(byte) = "camlp5,pa_ppx.deriving_plugins.show.link,camlp5.parser_quotations.link,pa_ppx_q_ast.runtime"
+  requires = "camlp5,pa_ppx.deriving_plugins.show.link,camlp5.parser_quotations.link,pa_ppx_q_ast.runtime"
   archive(byte) = "pa_deriving_q_ast.cmo"
   archive(native) = "pa_deriving_q_ast.cmx"
   )
