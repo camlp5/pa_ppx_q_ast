4bdcd5d50561ed59cdfaf33d15d602dc526fb10a
Chet Murthy <chetsky@gmail.com>
2023-02-26 23:38:55 -0800

    meh

--- pa_q_ast/pa_deriving_q_ast.ml
+++ pa_q_ast/pa_deriving_q_ast.ml
@@ -189,6 +189,7 @@ value generate_conversion arg rc rho in_patt (name, t) =
   | <:ctyp:< vala $ty$ >> | <:ctyp:< Ploc.vala $ty$ >> | <:ctyp:< Pcaml.t $ty$ >> ->
     <:expr< C.vala $genrec ty$ >>
 
+  | <:ctyp:< char >> -> <:expr< C.char >>
   | <:ctyp:< int >> -> <:expr< C.int >>
   | <:ctyp:< bool >> -> <:expr< C.bool >>
   | <:ctyp:< list >> -> <:expr< C.list >>
