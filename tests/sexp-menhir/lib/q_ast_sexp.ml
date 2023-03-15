b0493267f45e373d3192d863b54f4d44a6944c54
Chet Murthy <chetsky@gmail.com>
2023-03-14 19:45:18 -0700

    meh

--- tests/sexp-menhir/lib/q_ast_sexp.ml
+++ tests/sexp-menhir/lib/q_ast_sexp.ml
@@ -25,3 +25,57 @@ module Regular = struct
 }]
 
 end
+
+module Pattern = struct
+
+module MetaE = struct
+  include Q_ast_base.E_MetaSig
+  let int n = let loc = Ploc.dummy in <:expr< $int:string_of_int n$ >>
+  open Pa_ppx_base
+  open MLast
+    let xtr s = <:expr< $lid:s$ >>
+    let vala elem x =
+      match x with
+        Ploc.VaVal p -> elem p
+      | Ploc.VaAnt s -> xtr s
+end
+
+module MetaP = struct
+  include Q_ast_base.P_MetaSig
+  let int n = let loc = Ploc.dummy in <:patt< $int:string_of_int n$ >>
+  open Pa_ppx_base
+  open MLast
+    let xtr = function
+        "_" -> <:patt< _ >>
+       | s -> <:patt< $lid:s$ >>
+    let vala elem x =
+      match x with
+        Ploc.VaVal p -> elem p
+      | Ploc.VaAnt "_" -> <:patt< _ >>
+      | Ploc.VaAnt s -> xtr s
+end
+
+
+[%%import: Sexp.Pattern.sexp
+  [@add type location = [%import: Location.t]
+  ]
+  [@with Location.t := location]
+]
+[@@deriving q_ast {
+       default_data_source_module = Sexp.Normal
+     ; default_quotation_source_module = Sexp.Pattern
+     ; expr_meta_module = MetaE
+     ; patt_meta_module = MetaP
+     ; loc_mode = CustomLoc { loc_varname = __loc__ ; loc_type = [%typ: location] ; loc_function_name = location }
+     ; pertype = {
+         sexp = {
+           custom_branches_code = function
+           | Xtra (loc, s) → C.xtr s
+         }
+    }
+     ; entrypoints = [
+         { name = "pattern_sexp" ; from_string = Sexp_parse.pattern_sexp ; type_name = sexp }
+       ]
+}]
+
+end
