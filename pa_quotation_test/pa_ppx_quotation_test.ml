82bcb76a5dee447b006a63c45a83e5891a8a5a94
Chet Murthy <chetsky@gmail.com>
2024-07-06 18:02:51 -0700

    meh

--- pa_quotation_test/pa_ppx_quotation_test.ml
+++ pa_quotation_test/pa_ppx_quotation_test.ml
@@ -588,11 +588,24 @@ and expr_of_cons_decl0 rc (tdname, modli, (loc, c, _, tl, rto, _)) = do {
     let tl = Pcaml.unvala tl in
     let tnl = name_of_vars rc (fun t -> t) tl in
     let exprs1 (t, tn) =
-      let modli = match t with [
-            <:ctyp< { $list:_$ } >> -> Some modli
-          | _ -> None
-          ] in
-      expr_list_of_type_gen loc rc ~{tdname} tn ((modli, Some c), t) in
+      match t with [
+          <:ctyp< { $list:_$ } >> -> 
+          let el = expr_list_of_type_gen loc rc ~{tdname} tn ((Some modli, Some c), t) in
+          el
+          |> List.map (fun [
+            <:expr:< { $list:pel$ } >> ->
+              let pel = pel |> List.map (fun [
+                  (<:patt:< $longid:_$ . $lid:id$ >>, e) -> (<:patt< $lid:id$ >>, e)
+                | (<:patt< $lid:_$ >>, _) as pe -> pe
+                | _ -> assert False
+                ]) in
+              <:expr:< { $list:pel$ } >>
+          | _ -> assert False
+               ])
+        | _ ->
+           expr_list_of_type_gen loc rc ~{tdname} tn ((None, Some c), t)
+        ]
+    in
     let ell = List.map exprs1 tnl in
     let el = expr_list_cross_product ell in
     let mkapp l =
